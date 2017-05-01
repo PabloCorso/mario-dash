8b749b58427a3335429341cdda7ad73881106e1f
Pablo Corso <pabcorso@gmail.com>
2017-05-01 17:06:36 -0300

    Sacar la clase Tray.

--- src/GameState.hx
+++ src/GameState.hx
@@ -13,7 +13,6 @@ class GameState extends FlxState
 {
 	var map:FlxTilemap;
 	var player:Player;
-	var mTray:Tray;
 
 	public function new()
 	{
@@ -26,22 +25,6 @@ class GameState extends FlxState
 		map.loadMapFromCSV(Assets.getText(AssetPaths.mapCSV_map2_tiles__csv), AssetPaths.tiles__png, 32, 32, null, 0, 1, 1);
 		add(map);
 
-		var mapMayonnaise = new FlxTilemap();
-		mapMayonnaise.loadMapFromCSV(Assets.getText(AssetPaths.mapCSV_map2_tiles__csv), AssetPaths.tiles__png, 32, 32, null, 0, 1, 1);
-		for (y in 0...mapMayonnaise.heightInTiles)
-		{
-			for (x in 0...mapMayonnaise.widthInTiles)
-			{
-				if (mapMayonnaise.getTile(x, y) != 0)
-				{
-					mapMayonnaise.setTile(x, y, -1);
-				}
-			}
-		}
-
-		add(mapMayonnaise);
-		mTray = new Tray(mapMayonnaise);
-
 		player = new Player(this,100, 100);
 		add(player);
 
@@ -53,11 +36,6 @@ class GameState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-
 		FlxG.collide(map, player);
-
-		mTray.setContactPosition(player.x + player.width / 2, player.y + player.height + 1, FlxObject.DOWN);
-		mTray.setContactPosition(player.x -1, player.y + player.height/2, FlxObject.LEFT);
-		mTray.setContactPosition(player.x + player.width+1, player.y + player.height/2, FlxObject.RIGHT);
 	}
 }
