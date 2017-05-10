e6c878196ecc8b5f9d41a2832abd795b11d398f2
Pablo Corso <pabcorso@gmail.com>
2017-05-10 20:39:49 -0300

    Mejorar lista de botones en el menu.

--- src/states/MenuState.hx
+++ src/states/MenuState.hx
@@ -3,6 +3,7 @@ import controls.MapButton;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.ui.FlxButton;
+import flixel.util.FlxAxes;
 import gameObjects.MapData;
 import haxe.Json;
 import openfl.Assets;
@@ -28,12 +29,16 @@ class MenuState extends FlxState
 		var maps:Array<MapData> = Json.parse(mapsFile);
 
 		var i:Int;
-		for (i in 0...maps.length)
+		if (maps.length > 0)
 		{
-			var btn = new MapButton(0, 0, maps[i]);
-			btn.screenCenter();
-
-			add(btn);
+			var mult = FlxG.height / maps.length;
+			for (i in 0...maps.length)
+			{
+				var btn = new MapButton(0, 0, maps[i]);
+				btn.screenCenter(FlxAxes.X);
+				btn.y += i + 1 * 50;
+				add(btn);
+			}
 		}
 	}
 
